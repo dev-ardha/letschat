fbff23c4b8e5821abc969510bbe27b9124d74dd6
dev-ardha <ardhaydhtm@gmail.com>
2020-10-10 23:33:17 +0700

    Fix redux devtools extension issue

--- letschat-client/src/redux/store.js
+++ letschat-client/src/redux/store.js
@@ -1,6 +1,5 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 
 const initialState = {}
@@ -9,5 +8,9 @@ const middleware = [thunk]
 export const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+   
 )
